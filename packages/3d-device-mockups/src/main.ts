74d85134c8f54db188c485418463cce9ef7bd6f2
cuong.pham <cuong95nd@gmail.com>
2021-11-08 00:53:56 +0700

    refactor: remove preact, patch create-figma-plugin builder

--- packages/3d-device-mockups/src/main.ts
+++ packages/3d-device-mockups/src/main.ts
@@ -1,13 +1,17 @@
-import { loadFontsAsync, once, showUI } from '@create-figma-plugin/utilities'
+import { loadFontsAsync, once, showUI } from "@create-figma-plugin/utilities";
 
-import { InsertCodeHandler } from './types'
+import { InsertCodeHandler } from "./types";
 
 export default function () {
-  once<InsertCodeHandler>('INSERT_CODE', async function (code: string) {
-    const text = figma.createText()
-    await loadFontsAsync([text])
-    text.characters = code
-    figma.closePlugin()
-  })
-  showUI({ width: 320, height: 240 })
+  once<InsertCodeHandler>("INSERT_CODE", async function (code: string) {
+    const text = figma.createText();
+    await loadFontsAsync([text]);
+    text.characters = code;
+    figma.closePlugin();
+  });
+  figma.on("selectionchange", function () {
+    console.log(figma.currentPage.selection);
+  });
+
+  showUI({ width: 320, height: 240 });
 }
--- packages/3d-device-mockups/src/ui.tsx
+++ packages/3d-device-mockups/src/ui.tsx
@@ -1,51 +1,24 @@
-import {
-  Button,
-  Container,
-  render,
-  VerticalSpace
-} from '@create-figma-plugin/ui'
-import { emit } from '@create-figma-plugin/utilities'
-import { h } from 'preact'
-import { useCallback, useState } from 'preact/hooks'
-import { highlight, languages } from 'prismjs'
-import Editor from 'react-simple-code-editor'
+import { emit } from "@create-figma-plugin/utilities";
+import React, { useCallback, useState } from "react";
+import ReactDOM from "react-dom";
+import { InsertCodeHandler } from "./types";
 
-import styles from './styles.css'
-import { InsertCodeHandler } from './types'
-
-import 'prismjs/components/prism-clike.js'
-import 'prismjs/components/prism-javascript.js'
-import '!prismjs/themes/prism.css'
-
-function Plugin() {
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`)
+const App = () => {
+  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
   const handleInsertCodeButtonClick = useCallback(
     function () {
-      emit<InsertCodeHandler>('INSERT_CODE', code)
+      emit<InsertCodeHandler>("INSERT_CODE", code);
     },
     [code]
-  )
+  );
   return (
-    <Container>
-      <VerticalSpace space="small" />
-      <div class={styles.container}>
-        <Editor
-          highlight={function (code: string) {
-            return highlight(code, languages.js, 'js')
-          }}
-          onValueChange={setCode}
-          preClassName={styles.editor}
-          textareaClassName={styles.editor}
-          value={code}
-        />
-      </div>
-      <VerticalSpace space="large" />
-      <Button fullWidth onClick={handleInsertCodeButtonClick}>
-        Insert Code
-      </Button>
-      <VerticalSpace space="small" />
-    </Container>
-  )
-}
+    <div>
+      <button onClick={handleInsertCodeButtonClick}>AHIHI</button>
+    </div>
+  );
+};
 
-export default render(Plugin)
+export default ReactDOM.render(
+  <App />,
+  document.getElementById("create-figma-plugin")
+);

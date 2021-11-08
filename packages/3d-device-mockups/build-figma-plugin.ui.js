522fd79bce7d1e0403fd137d875be56489f6c25c
cuong.pham <cuong95nd@gmail.com>
2021-11-08 15:33:02 +0700

    refactor: correct way to build reactjs

--- packages/3d-device-mockups/build-figma-plugin.ui.js
+++ packages/3d-device-mockups/build-figma-plugin.ui.js
@@ -1,9 +1,17 @@
 module.exports = function (buildOptions) {
+  let loader = { ...buildOptions.loader, ".glb": "dataurl" };
+  let plugins = buildOptions.plugins.filter(
+    (plugin) => plugin.name != "preact-compat"
+  );
+
   return {
     ...buildOptions,
     define: {
       global: "window",
     },
-    loader: { ".glb": "dataurl" },
+    jsxFactory: undefined,
+    jsxFragment: undefined,
+    plugins: plugins,
+    loader: loader,
   };
 };
